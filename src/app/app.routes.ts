9c8eec351d31fb71869da374238bc169a5fdc57b
LeanmuraPTLY <leandro.murakoshi@partnersly.com>
2025-08-27 18:31:10 -0300

    tarea pt. 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {   // REDIRIGIR A OTRO PATH
+        path: '',
+        redirectTo: 'bienvenida',
+        pathMatch: 'full'
+    },
+    {
+        path: 'bienvenida',
+        loadComponent: () => import('./pages/bienvenida/bienvenida').then(archivo => archivo.Bienvenida)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login').then(archivo => archivo.Login)
+    },
+    {
+        path: 'registro',
+        loadComponent: () => import('./pages/registro/registro').then(archivo => archivo.Registro)
+    },
+    {
+        path: 'error',
+        loadComponent: () => import('./pages/error/error').then(archivo => archivo.Error)
+    },
+    {
+        path: 'sobre-mi',
+        loadComponent: () => import('./pages/sobre-mi/sobre-mi').then(archivo => archivo.SobreMi)
+    },
+    {   // CUALQUIER RUTA NO DEFINIDA
+        path: '**',
+        loadComponent: () => import('./pages/error/error').then(archivo => archivo.Error)
+    }
+];
